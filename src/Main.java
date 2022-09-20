93cc14b95af3424ca32128dca404cc7fe7ba51cf
ㅁin <2049905@naver.com>
2022-09-20 21:40:47 +0900

    1일차

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        int num1;
+        int num2;
+        num1 = 1;
+        num2 = 10;
+        System.out.println(num1);
+        System.out.println(num2);
         System.out.println("Hello world!");
     }
 }
+// 변수 이름은 숫자로 시작 X
+// $, _ 이외의 다른 특수문자는 변수의 이름에 사용 X
+// 키워드는 변수 이름 사용 X
