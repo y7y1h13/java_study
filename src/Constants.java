ac0d34b96306dedacb4199cbc928665211725f02
ㅁin <2049905@naver.com>
2022-09-21 21:26:48 +0900

    2일차

---
+++ src/Constants.java
@@ -0,0 +1,13 @@
+public class Constants {
+    public static void main(String[] args) {
+        final int MAX_SIZE = 100;
+        final char CONST_CHAR = '상';
+        final int CONST_ASSIGNED;
+        CONST_ASSIGNED = 12;
+        System.out.println(MAX_SIZE);
+        System.out.println(CONST_CHAR);
+        System.out.println(CONST_ASSIGNED);
+    }
+}
+
+// final을 붙이면 상수가 된다.
